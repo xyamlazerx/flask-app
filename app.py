2f45b7cc67a775d9e9aa925bc6ced968e4de9842
xyamlazerx <xyamlazerx@gmail.com>
2026-02-09 10:42:35 +0200

    adding ai route

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask! branch test"
+def ai():
+    return jsonify({
+		"invoke_ai"":"predict next buy"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
